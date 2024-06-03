67f111b19ecc280a67dc53f4c52271ede92572c3
joohyunje <joohj8677@naver.com>
2024-06-04 04:51:30 +0900

    5월 30일 mybatis 중

---
+++ workspace/c_mybatis/src/main/java/com/example/c_mybatis/Test/Test.java
@@ -0,0 +1,12 @@
+package com.example.c_mybatis.Test;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Test {
+
+    String name;
+    int age;
+}

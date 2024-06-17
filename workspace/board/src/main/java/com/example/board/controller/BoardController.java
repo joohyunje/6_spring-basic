9f536de11c28f1e558e81ffbb48d5abb72a58363
joohyunje <joohj8677@naver.com>
2024-06-17 21:52:25 +0900

    6월 17일 boardPro 진행중

--- workspace/board/src/main/java/com/example/board/controller/BoardController.java
+++ workspace/board/src/main/java/com/example/board/controller/BoardController.java
@@ -1,13 +1,13 @@
 package com.example.board.controller;
 
+import com.example.board.domain.dto.BoardDTO;
 import com.example.board.domain.dto.BoardListDTO;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -20,7 +20,7 @@ public class BoardController {
 
     @GetMapping("/list")
     public String list(@RequestParam(value = "pageNo", defaultValue = "1") int pageNo,
-                       @RequestParam(value = "pageSize", defaultValue = "2") int pageSize,
+                       @RequestParam(value = "pageSize", defaultValue = "7") int pageSize,
                        Model model) {
 
         int totalBoards = boardService.countBoard();
@@ -28,7 +28,7 @@ public class BoardController {
 
         List<BoardListDTO> boards = boardService.selectPaging(pageNo, pageSize);
 
-        int pageGroupSize = 3;
+        int pageGroupSize = 5;
         int startPage = ((pageNo - 1) / pageGroupSize) * pageGroupSize + 1;
         int endPage = Math.min(startPage + pageGroupSize - 1, totalPages);
 
@@ -42,6 +42,21 @@ public class BoardController {
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         return "board/list";
+    }
+
+    // 게시글 작성 폼으로 이동
+    @GetMapping("/write")
+    public String writeForm(Model model) {
+        model.addAttribute("board", new BoardDTO());
+        return "board/write";
+    }
+
+    @PostMapping("/write")
+    public String write(BoardDTO board, @RequestParam("providerId") String providerId,
+                        List<MultipartFile> files) {
+        board.setProviderId(providerId);
+        boardService.saveBoard(board, files);
+        return "redirect:/board/list";
     }
 
 }
